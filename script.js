6dae9e24012ce71943e70e83e08428de033979f0
NurgalievAl <100514113+NurgalievAl@users.noreply.github.com>
2022-02-27 15:16:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const alf = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя 0123456789";
+let k, x, z;
+let crypt;
+
+function GammaCoding() {
+  let source = document.getElementById('message').value;
+  let key = document.getElementById('gamma').value;
+  let res = '';
+                  
+  while (key.length < source.length) {
+    key += key;
+    if (key.length > source.length) {
+      key = key.substr(0, source.length);
+    }
+  }
+
+  for (let i = 0; i < source.length; i++) {
+    for (let id = 0; id < alf.length; id++) {
+      if (key[i] == alf[id]) {
+        k = id
+      };
+
+      if (source[i] == alf[id]) {
+        x = id
+      };
+
+      z = (x + k) % alf.length;
+    }
+    res += alf[z];
+  }
+  crypt=res;
+  alert(crypt);
+}
+
+function Decryption() {
+  let source = crypt;
+  let key = document.getElementById('gamma').value;            
+  res='';
+
+  while (key.length < source.length) {
+    key += key;
+    if (key.length > source.length) {
+      key = key.substr(0, source.length)
+    };
+  }
+
+  for (let i = 0; i < source.length; i++) {
+    for (let id = 0; id < alf.length; id++) {
+      if (key[i] == alf[id]) {
+        k = id
+      };
+
+      if (source[i] == alf[id]) {
+        x = id
+      };
+
+      z = ((x - k) + alf.length) % alf.length;
+    }
+    res += alf[z];
+  }
+  alert(res);
+}
