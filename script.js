4b714f36bb23a2f1052db123a6b2e3f1dc382d5b
NurgalievAl <100514113+NurgalievAl@users.noreply.github.com>
2022-03-01 17:16:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function GammaCoding() {
   let source = document.getElementById('message').value;
   let key = document.getElementById('gamma').value;
   let res = '';
-  if  ((source ==''|| source == undefined) && (key == '' || key == undefined)) return;                
+  if  ((source ==''|| source == undefined) || (key == '' || key == undefined)) return;                
   while (key.length < source.length) {
     key += key;
     if (key.length > source.length) {
@@ -35,7 +35,7 @@ function Decryption() {
   let source = document.getElementById('message').value;
   let key = document.getElementById('gamma').value;            
   res='';
-  if  ((source ==''|| source == undefined) && (key == '' || key == undefined)) return;           
+  if  ((source ==''|| source == undefined) || (key == '' || key == undefined)) return;           
   while (key.length < source.length) {
     key += key;
     if (key.length > source.length) {
